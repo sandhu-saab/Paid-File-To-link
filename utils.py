3322e948c162954d5e7deb4e53b7f80b3b73b06e
1234Rowdybaby <167402968+1234Rowdybaby@users.noreply.github.com>
2025-06-05 09:37:10 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 from shortzy import Shortzy
 from info import SHORTLINK_URL, SHORTLINK_API
+import json
+from datetime import datetime
 
 class temp(object):
     ME = None
@@ -7,8 +9,20 @@ class temp(object):
     U_NAME = None
     B_NAME = None
 
-
 async def get_shortlink(link):
     shortzy = Shortzy(api_key=SHORTLINK_API, base_site=SHORTLINK_URL)
     link = await shortzy.convert(link)
     return link
+
+# ✅ Premium check function
+def is_premium(user_id):
+    try:
+        with open("premium_users.json", "r") as f:
+            data = json.load(f)
+        expiry = data.get(str(user_id))
+        if not expiry:
+            return False
+        expiry_date = datetime.strptime(expiry, "%Y-%m-%d")
+        return expiry_date >= datetime.now()
+    except:
+        return False
