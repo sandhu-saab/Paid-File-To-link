aa226a7817606e23c10fc3873ef38f0fa9736599
akmoviebot <mrak7753@gmail.com>
2025-06-07 00:20:10 +0530

    Update plan.py

--- plugins/plan.py
+++ plugins/plan.py
@@ -4,11 +4,11 @@ from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, 
 @Client.on_message(filters.command("plan") & filters.private)
 async def show_plan_buttons(client, message: Message):
     buttons = InlineKeyboardMarkup([
-        [InlineKeyboardButton("🕐 1 Week ₹39", callback_data="plan_week"),
-         InlineKeyboardButton("📅 1 Month ₹69", callback_data="plan_month")],
-        [InlineKeyboardButton("📅 2 Months ₹149", callback_data="plan_2month"),
-         InlineKeyboardButton("📅 3 Months ₹199", callback_data="plan_3month")],
-        [InlineKeyboardButton("📆 1 Year ₹499", callback_data="plan_year")]
+        [InlineKeyboardButton("🕐 1 Week ₹9", callback_data="plan_week"),
+         InlineKeyboardButton("📅 1 Month ₹19", callback_data="plan_month")],
+        [InlineKeyboardButton("📅 2 Months ₹29", callback_data="plan_2month"),
+         InlineKeyboardButton("📅 3 Months ₹49", callback_data="plan_3month")],
+        [InlineKeyboardButton("📆 1 Year ₹99", callback_data="plan_year")]
     ])
 
     await message.reply_text(
@@ -20,11 +20,11 @@ async def show_plan_buttons(client, message: Message):
 @Client.on_callback_query(filters.regex("plan_"))
 async def plan_buttons(_, query: CallbackQuery):
     plans = {
-        "plan_week": ("🕐 1 Week Plan\n\n💰 Price: ₹39", "https://graph.org/file/5635f6bd5f76da19ccc70-695af75bfa01aacbf2.jpg"),
-        "plan_month": ("📅 1 Month Plan\n\n💰 Price: ₹69", "https://graph.org/file/5635f6bd5f76da19ccc70-695af75bfa01aacbf2.jpg"),
-        "plan_2month": ("📅 2 Months Plan\n\n💰 Price: ₹149", "https://graph.org/file/5635f6bd5f76da19ccc70-695af75bfa01aacbf2.jpg"),
-        "plan_3month": ("📅 3 Months Plan\n\n💰 Price: ₹199", "https://graph.org/file/5635f6bd5f76da19ccc70-695af75bfa01aacbf2.jpg"),
-        "plan_year": ("📆 1 Year Plan\n\n💰 Price: ₹499", "https://graph.org/file/5635f6bd5f76da19ccc70-695af75bfa01aacbf2.jpg")
+        "plan_week": ("🕐 1 Week Plan\n\n💰 Price: ₹9", "https://envs.sh/ftM.jpg"),
+        "plan_month": ("📅 1 Month Plan\n\n💰 Price: ₹19", "https://envs.sh/ftX.jpg"),
+        "plan_2month": ("📅 2 Months Plan\n\n💰 Price: ₹29", "https://envs.sh/ft6.jpg"),
+        "plan_3month": ("📅 3 Months Plan\n\n💰 Price: ₹49", "https://envs.sh/ftV.jpg"),
+        "plan_year": ("📆 1 Year Plan\n\n💰 Price: ₹99", "https://envs.sh/ftx.jpg")
     }
 
     plan = plans.get(query.data)
@@ -34,9 +34,9 @@ async def plan_buttons(_, query: CallbackQuery):
             photo=qr_url,
             caption=f"""{caption}
 
-📥 *Scan QR or pay using UPI ID*: `kingvj@ybl`
-👤 *Payee Name*: `VJ King`
-📩 *Send Payment Screenshot to*: [@Sandymaiwait](https://t.me/Sandymaiwait)""",
+📥 *Scan QR or pay using UPI ID*: `abhishek.0307-27@waicici`
+👤 *Payee Name*: `Abhishek kumar `
+📩 *Send Payment Screenshot to*: [@Tv_serial_wala](https://t.me/Tv_serial_wala)""",
             parse_mode="Markdown"
         )
     await query.answer()
